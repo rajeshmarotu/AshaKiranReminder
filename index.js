9bea63698ba079b5177743fe895a2a87b39ff6cb
rajeshmarotu <rajesh15@iiitkottayam.ac.in>
2018-02-06 12:04:30 +0530

    added time zone

--- index.js
+++ index.js
@@ -7,32 +7,38 @@ var CronJob = require('cron').CronJob;
 
   var json = JSON.parse(require('fs').readFileSync('userids.json', 'utf8'));
 
-  //console.log('faffu')
+  console.log('faffu')
   try {
-    new CronJob('45,58 9,22 * * 7,1-4',function(error){
+    new CronJob('17,18 22 * * 7,1-4',function(error){
+      // 06,08 22 * * 7,1-4
+
         login({
-            email:"",
-            password:""
+            email:"rajeshkumarmarotu@gmail.com",
+            password:"haira20"
                },function callback(error,api){
+                  if (error) return console.error(error);
                   var json = JSON.parse(require('fs').readFileSync('userids.json', 'utf8'));
                   var d = new Date();
                   var n = d.getDay();
                   //console.log(n);
-                  if (error) return console.error(error);
+                  american_n=n-1;
+                  var keys = Object.keys(json[american_n.toString()][0]);
+
                   for(var i=0;i<keys.length;i++){
                         //console.log(json["0"][0][keys[i]])
                         if(i==0){
                           var time="2:00 pm";
                         }else{
-                          var time="2:30 pm"  
+                          var time="2:30 pm"
                         }
-                        var slot=json[n.toString()][0][keys[i]];
+                        var slot=json[american_n.toString()][0][keys[i]];
                         for(var stud in slot){
                           console.log(slot[stud]["username"]+time+slot[stud]["subject"])
                           var msg={
                               body:"Hey! "+slot[stud]["username"]+" slot: "+i+" Subject: "+slot[stud]["subject"]+"At: AshaKiran"+"time: "+time
                                   }
                                   api.sendMessage(msg,slot[stud]["profile_id"])
+                                  console.log('message sent!')
                         }
                   }
               });
